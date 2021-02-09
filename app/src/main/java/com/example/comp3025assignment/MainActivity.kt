04a354e193378f327c3308cbcb2a376fd31c5829
alexblom89 <55286258+alexblom89@users.noreply.github.com>
2021-02-08 21:15:14 -0500

    Bind main activity using view binding and create onclick listener for login button.

--- app/src/main/java/com/example/comp3025assignment/MainActivity.kt
+++ app/src/main/java/com/example/comp3025assignment/MainActivity.kt
@@ -2,12 +2,22 @@ package com.example.comp3025assignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.comp3025assignment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Will direct user to the dashboard activity after login
+        binding.loginButton.setOnClickListener{
+            if(binding.usernameEditText.text.toString().isEmpty())
+                Toast.makeText(this, "Please enter your username!", Toast.LENGTH_LONG).show()
+        }
     }
 }
