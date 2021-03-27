5ef0a33ceef1bbf23703334a2105a5bb40825cb0
alexblom89 <55286258+alexblom89@users.noreply.github.com>
2021-03-27 01:12:30 -0400

    Create set list recyclerview layout file. Create background.xml and background_selected.xml drawable files. Create SetListRVAdapter.kt recyclerview adapter.

---
+++ app/src/main/java/com/example/comp3025assignment/SetListRVAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.comp3025assignment
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.comp3025assignment.models.Set
+
+class SetListRVAdapter (val context: Context,
+                        val sets : List<Set>,
+                        val itemListener : SetItemListener) : RecyclerView.Adapter<SetListRVAdapter.SetViewHolder>()
+{
+    inner class SetViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        val setNameTextView = itemView.findViewById<TextView>(R.id.setNameTextView)
+    }
+
+    interface SetItemListener {
+        fun setSelected(set : Set)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SetViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.item_set, parent, false)
+        return SetViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: SetViewHolder, position: Int) {
+        val set = sets[position]
+        with(holder) {
+            setNameTextView.text = set.name
+        }
+
+        holder.itemView.setOnClickListener {
+            itemListener.setSelected(set)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return sets.size
+    }
+
+}
