f67e90d66808aec96a8b3779645b77ecd05336c7
alexblom89 <55286258+alexblom89@users.noreply.github.com>
2021-03-28 23:05:55 -0400

    Create SetItemListViewModel.kt and SetItemViewModelFactory.kt

---
+++ app/src/main/java/com/example/comp3025assignment/SetItemListViewModel.kt
@@ -0,0 +1,39 @@
+package com.example.comp3025assignment
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.comp3025assignment.models.SetItem
+import com.google.firebase.firestore.FirebaseFirestore
+
+class SetItemListViewModel(setID: String) : ViewModel() {
+    private val setItems : MutableLiveData<List<SetItem>> by lazy {
+        MutableLiveData()
+    }
+
+    init {
+        val db = FirebaseFirestore.getInstance().collection("items")
+            .whereEqualTo("setID", setID)
+
+        db.addSnapshotListener { documents, exception ->
+            if (exception != null) {
+                Log.w("DBQUERY", "Listen Failed")
+                return@addSnapshotListener
+            }
+            documents?.let {
+                val setItemList = ArrayList<SetItem>()
+                for (document in documents)
+                {
+                    val item = document.toObject(SetItem::class.java)
+                    setItemList.add(item)
+                }
+                setItems.value = setItemList
+            }
+        }
+    }
+
+    fun getSetItems() : LiveData<List<SetItem>> {
+        return setItems
+    }
+}
---
+++ app/src/main/java/com/example/comp3025assignment/SetItemViewModelFactory.kt
@@ -0,0 +1,16 @@
+package com.example.comp3025assignment
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import java.lang.IllegalArgumentException
+
+class SetItemViewModelFactory(private val setID: String) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(SetItemListViewModel::class.java))
+        {
+            return SetItemListViewModel(setID) as T
+        }
+        else
+            throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
